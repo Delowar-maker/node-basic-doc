75fe038b079df6396cf5dd714d8b6926329b930b
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-08 22:33:27 +0600

    http clint

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World');
+    if (req.url === '/') {
+        // Setting the status code, status message, and headers
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        // Writing response body
+        res.write('<h1>this is the home page</h1>');
+        // Ending the response
+        res.end();
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>this is the about page</h1>');
+        res.end();
+    }
 });
 
 server.listen(3000);
