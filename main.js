6ee1be0f43c4d10dd7b76c70e50fb81f240046f0
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-09 02:46:53 +0600

    asynchronous read file

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const http = require('http');
-const URL = require('url');
+var fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    const myURL = 'http://localhost:8080/default.htm?year=2017&month=february';
-    const q = URL.parse(myURL, true);
-
-    console.log(q.host); //returns 'localhost:8080'
-    console.log(q.pathname); //returns '/default.htm'
-    console.log(q.search); //returns '?year=2017&month=february'
-
-    var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-    console.log(qdata.month); //returns 'february'
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
 });
 server.listen(3000);
 
 console.log('Server running on port 3000');
+
+//10
