986256482a8fd530129bd37601c1d1c3ec94c9a1
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-09 19:31:07 +0600

    File write Async

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const http = require('http');
 var fs = require('fs');
+const { error } = require('console');
 
 
 const server = http.createServer((req, res) => {
+    fs.writeFile('demo.txt', 'hello world', function (error, data) {
+        if (error) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('File Write Failed');
+            res.end();
+        }
+        else {
+            res.writeHead(400, { 'Content-Type': 'text/html' });
+            res.write('File Write Successfully');
+            res.end();
+        }
+    });
 
-    //Sync
-    const myData = fs.readFileSync('index.html');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(myData);
-    res.end();
 });
 server.listen(3000);
 
 console.log('Server running on port 3000');
-
-//15
