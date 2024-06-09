5b95451b502a8644da5a0c0a6b382eba4e7eff2a
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-09 19:39:42 +0600

    Sync write file

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const http = require('http');
 var fs = require('fs');
-const { error } = require('console');
+
 
 
 const server = http.createServer((req, res) => {
-    fs.writeFile('demo.txt', 'hello world', function (error, data) {
-        if (error) {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('File Write Failed');
-            res.end();
-        }
-        else {
-            res.writeHead(400, { 'Content-Type': 'text/html' });
-            res.write('File Write Successfully');
-            res.end();
-        }
-    });
+
+    let error = fs.writeFileSync('demoSync.txt', 'Welcome to Sync file write');
+    if (error) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('File Write Failed');
+        res.end();
+    }
+    else {
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.write('File Write Successfully');
+        res.end();
+    }
 
 });
 server.listen(3000);
