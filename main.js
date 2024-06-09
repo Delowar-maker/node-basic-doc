773664870342d6afb3517d59a43e8c58ad823d14
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-09 17:57:11 +0600

    Synchronous read file

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    fs.readFile('index.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
+
+    //Sync
+    const myData = fs.readFileSync('index.html');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(myData);
+    res.end();
 });
 server.listen(3000);
 
 console.log('Server running on port 3000');
 
-//10
+//15
